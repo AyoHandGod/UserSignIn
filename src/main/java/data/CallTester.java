c0bfafde5e4aea9b69329376a699145504bd3fdc
Gray King <Calikidd86@gmail.com>
2021-01-09 21:42:27 -0800

    Added Access and Refresh Token process to CallTester psvm. Confirmed that it's working, but will have to workout the best way to pass data from our servlet back to these methods.

--- src/main/java/data/CallTester.java
+++ src/main/java/data/CallTester.java
@@ -2,9 +2,13 @@ package data;
 
 import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.SpotifyHttpManager;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
+import org.apache.hc.core5.http.ParseException;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -25,9 +29,29 @@ public class CallTester {
                 .setRedirectUri(SpotifyHttpManager.makeUri("http://localhost:8080/UserSignIn_war_exploded/home"))
                 .build();
 
-        AuthorizationCodeUriRequest authCodeRequest = spotifyApi.authorizationCodeUri().build();
-        URI uri = authCodeRequest.execute();
+        AuthorizationCodeUriRequest authCodeUriRequest = spotifyApi.authorizationCodeUri().build();
+        URI uri = authCodeUriRequest.execute();
         String accessToken = spotifyApi.getAccessToken();
         System.out.println("URI: " + uri.toString());
+        // End Auth Code Access Process
+
+        // We use the Auth Code that we retrieved previously in order to obtain the access and refresh
+        // tokens.
+        // Start Access and Refresh Token Process
+        AuthorizationCodeRequest authorizationCodeRequest = spotifyApi
+                .authorizationCode("AQDN-fmL3IUcOw-9ap0Z1kLblz_UMvqjrpVCFr1Nrc81ivZXzRVbtHibjv89KTwkydFYb" +
+                        "0hQDOa-CrcC_1yJSKGnxLLtyqsYQZvLc88cvbIv7DhJj5eLe5w_l2yGx6VI6cAeB5eJUigoBi9S8UI5" +
+                        "AuQABRZqtHdHXFMVSMasED_c_M2FeVeP1XwrAR0hLeqHNP2qMYaF").build();
+
+        try {
+            AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
+            spotifyApi.setAccessToken(authorizationCodeCredentials.getAccessToken());
+            spotifyApi.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
+
+            System.out.println("Expires in: " + authorizationCodeCredentials.getExpiresIn());
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        // End Access and Refresh Token Process
     }
 }
